c1757dec08e54da7164fa7c008887c915000c58b
Ed Yu <eyu1@progressive.com>
2022-12-08 09:39:50 -0500

    update

--- BankingSolution/Banking.UnitTests/TestDoubles/StubbedBonusCalculator.cs
+++ BankingSolution/Banking.UnitTests/TestDoubles/StubbedBonusCalculator.cs
@@ -1,12 +1,12 @@
 ﻿
+namespace Banking.UnitTests.TestDoubles;
 
-namespace Banking.UnitTests.TestDoubles
+// Canned responses to questions.
+public class StubbedBonusCalculator : ICalculateBonuses
 {
-    public class StubbedBonusCalculator : ICalculateBonuses
+    public decimal GetBonusForDepositOn(decimal currentBalance, decimal amountOfDeposit)
     {
-        public decimal GetBonusForDepositOn(decimal currentBalance, decimal amountOfDeposit)
-        {
-            return currentBalance == 5000M && amountOfDeposit == 32.42M ? 42 : 0;
-        }
+        return currentBalance == 5000M && amountOfDeposit == 92.42M ? 42 : 0; 
     }
 }
+
